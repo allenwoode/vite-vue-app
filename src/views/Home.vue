b11a7c86c45ee5dff483e773592999a576cc2b7e
wufeilin <gfw.crack.out@gmail.com>
2024-06-26 20:51:31 +0800

    home view

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,7 +3,12 @@ import ChitChat from '../components/ChitChat.vue'
 </script>
 
 <template>
-    <div>
+    <div class="space-y-2">
+        <ChitChat />
+        <ChitChat />
+        <ChitChat />
+        <ChitChat />
+        <ChitChat />
         <ChitChat />
     </div>
 </template>
